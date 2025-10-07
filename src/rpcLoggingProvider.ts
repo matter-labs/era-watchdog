ff230dd1f643b13e10cb9b2e5029a447712fe0cf
Artur Puzio <contact@puzio.waw.pl>
2025-10-07 19:33:46 +0200

    feat: log properly all L2 json-rpc requests

--- src/rpcLoggingProvider.ts
+++ src/rpcLoggingProvider.ts
@@ -2,14 +2,13 @@ import winston from "winston";
 import { Provider as ZkSyncProvider } from "zksync-ethers";
 import { IBridgehub__factory } from "zksync-ethers/build/typechain";
 
-import type { Provider as EthersProvider } from "ethers";
+import { JsonRpcProvider as EthersJsonRpcProvider, type Provider as EthersProvider } from "ethers";
 import type { Fee, TransactionRequest } from "zksync-ethers/build/types";
 
 /**
  * Custom Provider wrapper that logs all JSON-RPC calls
  */
-export class LoggingZkSyncProvider extends ZkSyncProvider {
-  private requestId: number = 1;
+class ZkSyncOsProvider extends ZkSyncProvider {
   private l1Provider: EthersProvider | null = null;
   private isZKsyncOS = false;
 
@@ -51,47 +50,61 @@ export class LoggingZkSyncProvider extends ZkSyncProvider {
   /**
    * Override send method to intercept and log JSON-RPC calls
    */
-  override async send(method: string, params: unknown[] | Record<string, unknown>): Promise<unknown> {
-    const id = this.requestId++;
 
-    winston.debug(`[JSON-RPC Request] ID: ${id} Method: ${method}`, {
-      rpcRequest: {
-        id,
-        method,
-        params: JSON.stringify(params, (_, value) => (typeof value === "bigint" ? value.toString() : value)),
-      },
-    });
+}
 
-    const startTime = Date.now();
-    try {
-      // Call the parent class's send method directly
-      const result = await super.send(method, params);
-      const duration = Date.now() - startTime;
+type Ctor<T = {}> = new (...args: any[]) => T;
 
-      winston.debug(`[JSON-RPC Response] ID: ${id} Method: ${method} Duration: ${duration}ms`, {
-        rpcResponse: {
+
+const LoggingProviderMixing = <TBase extends Ctor<EthersJsonRpcProvider>>(Base: TBase) => {
+  return class LoggingProvider extends Base {
+    private requestId: number = 1;
+
+    override async send(method: string, params: unknown[] | Record<string, unknown>): Promise<unknown> {
+      const id = this.requestId++;
+
+      winston.debug(`[JSON-RPC Request] ID: ${id} Method: ${method}`, {
+        rpcRequest: {
           id,
           method,
-          result: JSON.stringify(result, (_, value) => (typeof value === "bigint" ? value.toString() : value)),
+          params: JSON.stringify(params, (_, value) => (typeof value === "bigint" ? value.toString() : value)),
         },
       });
 
-      return result;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      const duration = Date.now() - startTime;
+      const startTime = Date.now();
+      try {
+        // Call the parent class's send method directly
+        const result = await super.send(method, params);
+        const duration = Date.now() - startTime;
 
-      winston.error(`[JSON-RPC Error] ID: ${id} Method: ${method} Duration: ${duration}ms Error: ${error.message}`, {
-        rpcError: {
-          id,
-          method,
-          error: error.message,
-          code: error.code,
-          data: error.data,
-        },
-      });
+        winston.debug(`[JSON-RPC Response] ID: ${id} Method: ${method} Duration: ${duration}ms`, {
+          rpcResponse: {
+            id,
+            method,
+            result: JSON.stringify(result, (_, value) => (typeof value === "bigint" ? value.toString() : value)),
+          },
+        });
 
-      throw error;
+        return result;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        const duration = Date.now() - startTime;
+
+        winston.error(`[JSON-RPC Error] ID: ${id} Method: ${method} Duration: ${duration}ms Error: ${error.message}`, {
+          rpcError: {
+            id,
+            method,
+            error: error.message,
+            code: error.code,
+            data: error.data,
+          },
+        });
+
+        throw error;
+      }
     }
   }
 }
+
+export const LoggingZkSyncProvider = LoggingProviderMixing(ZkSyncOsProvider);
+export const LoggingEthersJsonRpcProvider = LoggingProviderMixing(EthersJsonRpcProvider);
